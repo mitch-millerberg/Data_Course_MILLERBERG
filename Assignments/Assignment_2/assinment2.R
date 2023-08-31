8bad82a104c91a6eed5ae284d0d34662eb933225
mitch-millerberg <mitchell.millerberg@gmail.com>
2023-08-31 15:00:40 -0600

    update

--- Assignments/Assignment_2/assinment2.R
+++ Assignments/Assignment_2/assinment2.R
@@ -1,8 +1,9 @@
 #assingment2 
 csv_files <- list.files(path = "Data",pattern = ".csv")
-csv_files
+
 length(csv_files)
 length_CSV <- length(csv_files)
+
 list.files(path = "Data",pattern = ".csv")
 #wingspan_vs Mass
 df <- read.csv("Data/wingspan_vs_mass.csv")
