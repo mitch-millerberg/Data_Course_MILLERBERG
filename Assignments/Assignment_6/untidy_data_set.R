21a1ea91ec11f6d9e0457929bbd833a14f9c5e95
mitch-millerberg <mitchell.millerberg@gmail.com>
2023-09-29 13:34:23 -0600

    start of ass_6

---
+++ Assignments/Assignment_6/untidy_data_set.R
@@ -0,0 +1,6 @@
+#Assignment 6
+#mitchell Millerberg
+#In this assignment, you will use R (within R-Studio) to:
+# 1) Load an untidy data set
+# 2) Tidy it using tidyr and dplyr verbs
+# 3) Plot it with ggplot
